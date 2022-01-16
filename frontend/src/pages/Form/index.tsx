7ebd220fe65ed80f5a9957adf7f3b9939d3c86d7
Junia Winner Windows <junia.winner@gmail.com>
2022-01-16 03:40:49 -0300

    useParams

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,45 +1,12 @@
-import { Link } from 'react-router-dom';
-import './styles.css';
+import FormCard from 'components/FormCard';
+import { useParams } from 'react-router-dom';
 
 function Form() {
 
-    const movie = {
-        id: 1,
-        image: "https://rederuasp.files.wordpress.com/2017/08/cropped-13041255_1022453481173057_7646186484742796767_o.jpg",
-        title: "Não Somos Mendigos",
-        count: 2,
-        score: 4.5
-    };
+    const params = useParams();
     
     return (
-    <div className="dsmovie-form-container">
-    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-    <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <form className="dsmovie-form">
-            <div className="form-group dsmovie-form-group">
-                <label htmlFor="email">Informe seu email</label>
-                <input type="email" className="form-control" id="email" />
-            </div>
-            <div className="form-group dsmovie-form-group">
-                <label htmlFor="score">Informe sua avaliação</label>
-                <select className="form-control" id="score">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            <div className="dsmovie-form-btn-container">
-                <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
-            </div>
-        </form >
-        <Link to="/">
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-        </Link>
-    </div >
-</div >
+       <FormCard movieId={`${params.movieId}`} />
     );
 }
 
